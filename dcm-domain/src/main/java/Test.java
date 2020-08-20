1e0d0d668b80e047c65e56f890b3e3ed061f46d1
yaoximing <yaoximing1356@100.me>
2020-08-20 17:18:05 +0800

    init

---
+++ dcm-domain/src/main/java/Test.java
@@ -0,0 +1,17 @@
+/**
+ * @author : yaoximing
+ * @date : 2020/8/20
+ * @description :
+ **/
+public class Test {
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(11);
+    }
+}
