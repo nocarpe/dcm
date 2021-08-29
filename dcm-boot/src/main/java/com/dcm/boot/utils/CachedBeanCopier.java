8893ffd1ddfd3fc1cac021ff92ba3a8e65b4b6f5
yaoximing <yaoximing1356@100.me>
2021-08-29 17:37:37 +0800

    git update

---
+++ dcm-boot/src/main/java/com/dcm/boot/utils/CachedBeanCopier.java
@@ -0,0 +1,26 @@
+package com.dcm.boot.utils;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.cglib.beans.BeanCopier;
+
+public class CachedBeanCopier {
+  
+    static final Map<String, BeanCopier> BEAN_COPIERS = new ConcurrentHashMap<>();
+  
+    public static void copy(Object srcObj, Object destObj) {
+        String key = genKey(srcObj.getClass(), destObj.getClass());
+        BeanCopier copier = null;
+        if (!BEAN_COPIERS.containsKey(key)) {  
+            copier = BeanCopier.create(srcObj.getClass(), destObj.getClass(), false);  
+            BEAN_COPIERS.put(key, copier);  
+        } else {  
+            copier = BEAN_COPIERS.get(key);  
+        }  
+        copier.copy(srcObj, destObj, null);  
+    }  
+  
+    private static String genKey(Class<?> srcClazz, Class<?> destClazz) {
+        return srcClazz.getName() + destClazz.getName();  
+    }  
+}  
